d009ff406f43a3daa2e9517205e2cacb02046580
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-03-31 20:54:34 -0400

    Adding HarvestCloudGeoBundle

---
+++ HarvestCloudGeoBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\GeoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * HarvestCloudGeoBundle
+ *
+ * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+ * @since  2014-03-30
+ */
+class HarvestCloudGeoBundle extends Bundle
+{
+}
