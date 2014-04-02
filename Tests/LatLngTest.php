af86b0e51a232efc2b31fa5e37760ccc65d17161
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-04-02 13:44:28 -0400

    Added first test

---
+++ Tests/LatLngTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\GeoBundle\Tests;
+
+use HarvestCloud\GeoBundle\LatLng;
+
+/**
+ * LatLngTest
+ *
+ * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+ * @since  2014-04-02
+ */
+class LatLngTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * testConstructor()
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-04-02
+     */
+    public function testConstructor()
+    {
+        $latLng = new LatLng(5,10);
+
+        $this->assertEquals(5, $latLng->getLatitude());
+        $this->assertEquals(10, $latLng->getLongitude());
+    }
+}
